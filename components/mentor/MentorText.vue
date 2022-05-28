c750bd54a92813552fba44b9a51cd4d958a7fc5e
Filipe Gomide <fgomiden@gmail.com>
2022-05-28 14:40:17 -0300

    chore: mentor page

---
+++ components/mentor/MentorText.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="text-gray-500 text-justify mx-auto px-10 px-md-40 max-w-4xl">
+    <p class="pb-4">
+      Francisco de Menezes Dias da Cruz, natural da cidade do Rio de Janeiro,
+      filho de antecedente de igual nome (chefe do Partido Liberal no Rio e
+      professor da Faculdade de Medicina) e de D. Rosa de Lima Dias da Cruz,
+      nasceu a 27 de fevereiro de 1853. Foi professor de Matemática no Colégio
+      Pinheiro, no qual concluíra o curso de humanidades.
+    </p>
+    <p class="pb-4">
+      Era, nessa época, aluno da Escola de Medicina, durante a qual contraiu
+      núpcias com a Exma. Sra. D. Adelaide Pinheiro Dias da Cruz. Ao formar-se
+      em Medicina, perdeu o pai, que havia sido ferido à baioneta na Igreja do
+      Sacramento. Foi bibliotecário durante dez anos da Câmara Municipal, sendo
+      demitido ao ser proclamada a República, sob a falsa imputação de
+      monarquista. Presidiu o Curso Hahnemaniano e o Instituto Hahnemaniano do
+      Brasil.
+    </p>
+    <p class="pb-4">
+      Possuidor de enorme clínica, o Dr. Dias da Cruz não fugia aos deveres da
+      caridade, dando, assim expansão aos seus sentimentos humanitários. Homem
+      de grande e invulgar cultura, deixou riquíssima biblioteca. Estudioso
+      desde a infância, preocupou-se com a ciência homeopática e, mais tarde,
+      diante de provas irrefutáveis tornou-se espírita dos mais caridosos e
+      evangélicos. É interessante relatar, ainda que superficialmente, a maneira
+      por que se verificou sua conversão. Tendo chegado ao seu conhecimento que
+      o espírito de seu genitor desenvolvia largo programa de caridade, através
+      de médiuns receitistas, decidiu ele, homem austero e cultor da verdade, ir
+      à Federação Espírita Brasileira para observar e apurar quanto de real
+      pudesse haver em torno da informação recebida.
+    </p>
+    <p class="pb-4">
+      Iniciada a reunião com a prece habitual, passou-se ao estudo doutrinário.
+      Até então nada ocorrera suscetível de lhe permitir aceitar a sessão das
+      manifestações atribuídas ao espírito de seu pai. Já estava propenso a
+      acreditar em mistificação, quando, à mesa que dirigia os trabalhos, um
+      médium demonstrou haver caído em transe. Era, afinal, a tão desejada
+      manifestação que inesperadamente se realizava. Através do médium, o
+      Espírito do primeiro Dr. Dias da Cruz pediu que chamassem seu filho, que
+      ali se encontrava no meio dos assistentes. Surpreso, este se aproximou,
+      incrédulo. A um dado momento, porém, seu genitor disse-lhe: Você se lembra
+      daquele fato que ocorreu conosco, na praça tal?
+    </p>
+    <p class="pb-4">
+      E, a seguir, revelou uma ocorrência só de ambos conhecida. Diante disso, o
+      Doutor Dias da Cruz (filho) sentiu chegada a hora de se render à
+      inelutável evidência. Ninguém o conhecia naquela assembleia e o fato
+      referido pelo espírito era absolutamente desconhecido de toda a sua
+      família, pois somente os dois dele haviam tido o conhecimento.
+    </p>
+    <p class="pb-4">
+      Percebeu, então, que ao seu caráter íntegro e probo só havia um caminho:
+      aceitar a veracidade da manifestação espirítica de seu genitor. E fê-lo
+      sem constrangimento, com a simpleza natural das almas puras. Pôs-se a
+      estudar o Espiritismo, enfronhou-se na interpretação dos textos
+      doutrinários e passou a ser, daí por diante, um novo e valoroso servidor
+      do Cristo nas fileiras dos seguidores de Kardec.
+    </p>
+    <p class="pb-4">
+      Em 1885, pronuncia na Federação Espírita Brasileira a sua primeira
+      conferência, e desde então participou de várias comissões importantes de
+      defesa do Espiritismo (1890, 1892 e 1893).
+    </p>
+    <p class="pb-4">
+      Em 1890, em substituição ao Dr. Bezerra de Menezes, foi, então, o Dr.
+      Francisco de Menezes Dias da Cruz, que anteriormente ocupava a
+      vice-presidência, eleito presidente da Federação Espírita Brasileira,
+      cargo que exerceu com devotamento até os primeiros dias de 1895, quando
+      foi substituído, temporariamente, por Júlio César de Menezes, o “Kardec
+      brasileiro”, seu colega de profissão e amigo.
+    </p>
+    <p class="pb-4">
+      Sob a sua presidência foram iniciados os trabalhos de socorro material e
+      espiritual da Assistência aos Necessitados, que até hoje constituem o
+      cerne dos serviços cristãos prestados pela Federação Espírita Brasileira.
+      Muitos foram os delicados companheiros que o ajudaram nessa obra
+      grandiosa, mantida e desenvolvida com o maior carinho pela Casa de Ismael,
+      sendo justo salientarmos, de passagem, o nome do confrade Bernardino
+      Cardoso, o qual lhe entregava mensalmente a quantia de um conto de réis.
+      Elevada importância para aqueles tempos (mais de 300 dólares), a fim de
+      que fosse distribuída com os pobres de sua clínica, sob a condição de lhe
+      não revelar o nome.
+    </p>
+    <p class="pb-4">
+      Em 1896, por proposta de Bezerra de Menezes, e em atenção aos abnegados
+      serviços prestados à Federação Espírita Brasileira, foi Dias da Cruz
+      aclamado presidente honorário da mesma.
+    </p>
+    <p class="pb-4">
+      Dirigiu o Reformador durante o período da sua presidência e escreveu
+      inúmeros artigos doutrinários e de polêmica com assinatura modesta de “Um
+      espírita”. É também autor do livro “O Professor Lombroso e o Espiritismo
+      “.
+    </p>
+    <p class="pb-4">
+      Foi quem primeiro tentou, em 1891, adquirir um prédio próprio par a FEB e
+      montar oficina tipográfica para a impressão do “Reformador” e de obras
+      espíritas em geral.
+    </p>
+    <p class="pb-4">
+      Este segundo Dias da Cruz foi, portanto, vice-presidente e presidente da
+      Federação durante muitos anos, desencarnando na cidade do Rio de Janeiro,
+      em 30 de setembro de 1937, na avançada idade de 84 anos. Gloriosa
+      ancianidade essa, atingido após proveitoso dispêndio de energias em favor
+      do próximo.
+    </p>
+    <p class="pb-4">
+      Em 1900, o Dr. Dias da Cruz reorganiza, ressuscita o “Instituto
+      Hahnemaniano do Brasil “, que havia sido criado em 1878 pelo mais afamado
+      médico homeopatia do Império, o Dr. Saturnino Soares de Meireles, seu
+      primeiro presidente. Dias da Cruz alugou no centro da cidade, à rua da
+      Quitanda n.º 59, uma casa para seu consultório, e neste reinstalou o
+      Instituto Hahnemaniano do Brasil. Por alguns anos os membros do Instituto
+      ali se reuniram, datando dessa época um novo ciclo de grandes atividades e
+      realizações.
+    </p>
+    <p class="pb-4">
+      Após a morte do Dr. Joaquim Murtinho, subiu à presidência do Instituto,
+      por um ano, o Dr. Teodoro Gomes. Substituiu-o o Dr. Licínio Cardoso, sob a
+      vice-presidência do Dr. Dias da Cruz. Esse foi o período áureo da
+      Homeopatia no Brasil, e frisa um historiador que ao Dr. Dias da Cruz cabe
+      grande parcela das glórias que o Instituto conquistou durante a
+      presidência do Dr. Licínio Cardoso.
+    </p>
+    <p class="pb-4">
+      Os “Anais de Medicina Homeopática “, cuja publicação fora interrompida em
+      1884, reapareceram em janeiro de 1901, devido aos esforços do “mais puro
+      dos homeopatas brasileiros”, o Dr. Dias da Cruz, que arrancou a revista do
+      Instituto do túmulo onde jazia, dando-lhe lugar honroso entre as
+      publicações periódicas sobre Medicina. Dela foi ele redator de 1901 a
+      1902, e de 1906 a 1910.
+    </p>
+    <p class="pb-4">
+      Ficou célebre a polêmica (1900-1901) entre o doutor Dias da Cruz e o Dr.
+      Nuno de Andrade, Diretor Geral de Saúde Pública, médico alopata e acirrado
+      inimigo da Homeopatia, o qual acabou por ser exonerado do cargo que
+      ocupava.
+    </p>
+    <p class="pb-4">
+      Fundada em 1912, a Faculdade Hahnemaniana (posteriormente denominada
+      Escola de Medicina e Cirurgia, com sede atual à rua Frei Caneca), Dias da
+      Cruz colaborou na organização dos programas de ensino do novel
+      estabelecimento, no qual lecionou a cadeira da Farmacologia e, mais tarde,
+      a 1ª cadeira de Matéria Médica, constituindo-se em verdadeiro mestre de
+      toda uma nova geração.
+    </p>
+    <p class="pb-4">
+      Dias da Cruz foi por muitos anos o orador oficial do Instituto. Sua
+      eloquência e seu saber impressionavam a todos. Quando da inauguração do
+      Hospital Hahnemaniano, em 1916, discursou brilhantemente em nome do
+      Instituto, ante numerosa e ilustrada assistência, presentes Licínio
+      Cardoso, Carlos Maximiliano, Ministro da Justiça, o Barão de Brasílio
+      Machado, Presidente do Conselho Superior do Ensino, o Dr. Paulo de
+      Frontin, Diretor da Escola Politécnica, e representantes do Presidente da
+      República e de Ministérios em geral.
+    </p>
+    <p class="pb-4">
+      Em 1926, o Dr. Licínio Cardoso pede demissão da presidência do Instituto,
+      sendo eleito, para substituí-lo, o Dr. Francisco de Menezes Dias da Cruz.
+      Este exerceu o cargo de Presidente efetivo até 29 de janeiro de 1930.
+      Nesse dia, reunido o Instituto em sessão extraordinária, foi aclamado
+      presidente-perpétuo o Dr. Dias da Cruz, após este haver renunciado, por
+      motivo de saúde, ao cargo de Presidente para o qual acabava de ser
+      reeleito. “Sua aclamação” - escreveu um historiador - “foi um direito
+      conquistado por seu valor mora, sua capacidade intelectual e, sobretudo,
+      por firmeza de suas convicções homeopáticas”.
+    </p>
+    <p class="pb-4">
+      De 25 a 30 de setembro de 1926 foi realizado o 1º Congresso Brasileiro de
+      Homeopatia, sob a presidência do Dr. Dias da Cruz. Propagandista dos mais
+      convictos e autorizados, possuidor de excelente cultura médica, mestre
+      reconhecido pela sua proficiência, com vasta clínica em que abundaram
+      notabilíssimas curas, constituiu ele, por mais um século, “um dos grandes
+      marcos no progresso da Homeopatia no Brasil”. “Não erramos afirmando“-
+      escreveu o Dr. José Emígdio Rodrigues Galhardo - “ser o Dr. Dias da Cruz,
+      entre os homeopatas brasileiros, aquele que maiores e mais perfeitos
+      conhecimentos tem da doutrina hahnemanniana.
+    </p>
+    <p class="pb-4">
+      Dizem os seus contemporâneos que o cumprimento do dever era quase que
+      sagrado para o Dr. Dias da Cruz. Como professor, jamais deixou de
+      comparecer à hora certa em suas aulas. Como clínico no Hospital
+      Hahnemaniano, não se fazia esperar pelos doentes. Eis em síntese, a
+      brilhante personalidade daquele que dignificou o Espiritismo e a
+      Homeopatia no Brasil.
+    </p>
+    <p class="pb-4">Fonte: Wantuil, Zêus. Grandes Espíritas do Brasil</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MentorText',
+}
+</script>
---
+++ pages/mentor/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="py-2 bg-white">
+    <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h2
+        class="
+          text-center text-3xl
+          leading-8
+          font-extrabold
+          tracking-tight
+          text-gray-900
+        "
+      >
+        Mentor
+      </h2>
+    </div>
+    <div class="mx-auto">
+      <div class="flex justify-center">
+        <div class="px-5 md:px-40">
+          <div class="flex flex-col text-center">
+            <h3 class="font-bold text-xl">Dr. Dias da Cruz</h3>
+          </div>
+          <div class="flex flex-col py-4">
+            <img
+              src="https://res.cloudinary.com/ame-rio/image/upload/v1653755796/Dias-da-Cruz-FIlho.jpg"
+              class="w-full h-52 object-contain"
+              alt="Dr. Dias da Cruz"
+            />
+          </div>
+        </div>
+      </div>
+      <MentorText />
+    </div>
+  </div>
+</template>
+<script>
+import MentorText from '~/components/mentor/MentorText.vue'
+export default {
+  components: { MentorText },
+}
+</script>
