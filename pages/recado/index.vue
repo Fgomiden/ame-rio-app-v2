8b6a74fc66a61aced683c2ec4031c3e66dae10ec
Filipe Gomide <fgomiden@gmail.com>
2022-05-28 14:40:38 -0300

    chore: recado page

---
+++ pages/recado/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="py-2 bg-white">
+    <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h2
+        class="
+          text-center text-3xl
+          leading-8
+          font-extrabold
+          tracking-tight
+          text-gray-900
+        "
+      >
+        Recado Fraterno
+      </h2>
+    </div>
+    <div class="text-gray-500 text-justify mx-auto px-10 px-md-40 max-w-4xl">
+      <div class="flex flex-col justify-center">
+        <p class="py-2">Recado fraterno aos associados, colegas e confrades:</p>
+        <p class="py-2">
+          É importante que nos integremos cada vez mais nessa doutrina que nos
+          traz o Cristo de volta. Acima de todas as convenções humanas, fomos
+          outorgados como servidores de jesus. Importante que nos mantenhamos
+          vigilantes e dispostos sempre em busca da nossa renovação espiritual.
+        </p>
+        <p class="py-2">
+          O momento que vivemos é muito difícil, o sofrimento assedia o homem. É
+          importante que nossa mente vibre permanentemente em harmonia,
+          refletindo paz e saúde. Assim fazendo, estaremos aptos a ajudar o
+          nosso semelhante e a nós mesmos igualmente necessitados.
+        </p>
+        <p class="py-2">
+          O nosso site (www.Ame-rio.Org.Br) e e-mail (amecgs@gmail.Com) são
+          inundados de solicitações de amparo por irmãos em humanidade, passando
+          por sofrimentos atrozes, vivenciando sérias crises no campo íntimo e
+          físico, buscando palavras de apoio e de incentivo para se reerguerem.
+        </p>
+        <p class="py-2">
+          Nossa instituição tem a direção maior do venerando espírito dr. Adolfo
+          bezerra de menezes, cujo secretário, o espírito dias da cruz, nos
+          acompanha e guia os nossos passos.
+        </p>
+        <p class="py-2">
+          Apesar de tantas lutas e obstáculos, desde a sua fundação há 22 anos,
+          a ame-rio se mantém fiel a jesus e a kardec. Sabemos que o compromisso
+          assumido por todos nós, na erraticidade, foi de servir com denodo e
+          coragem o nosso excelso cristo, colocando-o acima de nossas
+          imperfeições e das convenções humanas. Acima de qualquer título ou
+          rótulo, o mais valioso e autêntico é o de sermos servidor do cristo.
+        </p>
+        <p class="py-2">
+          Quando retornarmos à verdadeira pátria, o que carregaremos na bagagem?
+          Ilusão, vaidade, presunção descabida, egoísmo, orgulho ou a certeza do
+          dever cumprido? Urge, se for o caso, retomarmos o caminho da
+          singeleza, da humildade, da mansidão. Jesus, nosso guia maior, caminho
+          da verdade e da vida, está nos observando e espera de todos nós que
+          estejamos engolfados com ele no trabalho redentor de nossa reeducação
+          espiritual e de nossos semelhantes.
+        </p>
+        <p class="py-2">
+          Quando retornarmos à verdadeira pátria, o que carregaremos na bagagem?
+          Ilusão, vaidade, presunção descabida, egoísmo, orgulho ou a certeza do
+          dever cumprido? Urge, se for o caso, retomarmos o caminho da
+          singeleza, da humildade, da mansidão. Jesus, nosso guia maior, caminho
+          da verdade e da vida, está nos observando e espera de todos nós que
+          estejamos engolfados com ele no trabalho redentor de nossa reeducação
+          espiritual e de nossos semelhantes.
+        </p>
+        <p class="py-2">
+          Que o amado Mestre nos ampare sempre e que cada vez mais sejamos
+          merecedores de pertencer aos quadros do consolador prometido-a excelsa
+          doutrina espírita. Compareçam às nossas Reuniões
+          Científico-Doutrinárias, aos terceiros sábados do mês, no auditório da
+          AEOB (Associação Espirita Obreiros do Bem), situado na Rua Santa
+          alexandrina, 667, Rio Comprido, RJ, às 10:00h, estacionamento próprio.
+        </p>
+        <p class="py-2">
+          Salve Jesus! Salve Kardec! Salve todos os trabalhadores da Seara
+          Divina!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
